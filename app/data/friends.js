72add3607d80bc846bd7dac2cc1ede0e4ac6ee55
LoganPell <logan.pell@gmail.com>
2018-03-14 12:49:59 -0500

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+var friends = [
+  {
+    name: "Bill",
+    photo: "https://www.facebook.com/photo.php?fbid=1721414177903439&set=a.135986503112889.15952.100001046197237&type=3&theater",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Matt",
+    photo: "https://www.facebook.com/photo.php?fbid=10214977393491124&set=a.1610253419049.84848.1317647098&type=3&theater",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Grant",
+    photo: "https://www.facebook.com/photo.php?fbid=10209419923960018&set=a.1588038305844.79309.1382413943&type=3&theater",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Paul",
+    photo: "https://www.facebook.com/photo.php?fbid=10157616510915431&set=a.10150288595210431.554305.896625430&type=3&theater",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Trevor",
+    photo: "https://www.facebook.com/photo.php?fbid=590567624316515&set=a.148503351856280.24751.100000898495754&type=3&theater",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Dustin",
+    photo: "https://www.facebook.com/photo.php?fbid=1477383025675875&set=a.106756626071862.15295.100002125768741&type=3&theater",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+];
+
+module.exports = friends;
